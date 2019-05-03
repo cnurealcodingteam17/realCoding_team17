b9de90b595403d6d4668aaf61231fc59401a2771
kwakjuheon <juhun2005@naver.com>
2019-05-03 23:29:51 +0900

    리스트로 테스트하기 추가함

--- BookManagement/src/test/java/service/MockServiceTest.java
+++ BookManagement/src/test/java/service/MockServiceTest.java
@@ -1,6 +1,7 @@
 package service;
 
 import domain.Book;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -25,6 +26,22 @@ public class MockServiceTest{
     @InjectMocks
     private MockService mockService;
 
+    private List<Book> books = new ArrayList<Book>();
+
+
+    @Before
+    public void setUp(){
+
+        // 5개의 책 객체를 만듭니다.
+        Book book1 = new Book("Cinderella", "CharlesPerrault", 10000);
+        Book book2 = new Book("WinnieThePooh", "AlanAlexanderMilne", 15000);
+        Book book3 = new Book("Pinocchio", "CarloCollodi", 12500);
+
+        //앞서 만든 List 에 책을 추가합니다.
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
+    }
 
     @Test
     public void bookName_Cinderella_verify(){
@@ -74,4 +91,18 @@ public class MockServiceTest{
 
     }
 
+    // 책 리스트를 만들어 올바르게 저장되고 있는지 확인한다.
+    @Test
+    public void checkBookList(){
+        assertThat(books.get(2).getPrice(), is(12500));
+        assertThat(books.get(0).getName(), is("Cinderella"));
+        assertThat(books.get(1).getAuthor(), is("AlanAlexanderMilne"));
+    }
+
+    // 위의 테스트와 리스트를 같이 사용하여 test 해보기
+    @Test
+    public void combineTwo(){
+        when(mockService.findByName("Cinderella")).thenReturn(new Book("Cinderella", "AlanAlexanderMilne", 15000));
+        assertThat(mockService.findByName("Cinderella").getPrice(), is(books.get(1).getPrice()));
+    }
 }
