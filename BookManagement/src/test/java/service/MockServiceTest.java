a6bc38280686a8851e28062b2a22fac9811571c8
Kuuuna <yunaghgh@naver.com>
2019-05-04 17:23:46 +0900

    to sell books in a bookstore; Test to give change to customer

--- BookManagement/src/test/java/service/MockServiceTest.java
+++ BookManagement/src/test/java/service/MockServiceTest.java
@@ -1,6 +1,7 @@
 package service;
 
 import domain.Book;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,6 +13,8 @@ import repository.MockRepository;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
 
@@ -23,6 +26,22 @@ public class MockServiceTest{
     @InjectMocks
     private MockService mockService;
 
+    private List<Book> books = new ArrayList<Book>();
+
+
+    @Before
+    public void setUp(){
+
+        // 5개의 책 객체를 만듭니다.
+        Book book1 = new Book("Cinderella", "CharlesPerrault", 10000);
+        Book book2 = new Book("WinnieThePooh", "AlanAlexanderMilne", 15000);
+        Book book3 = new Book("Pinocchio", "CarloCollodi", 12500);
+
+        //앞서 만든 List 에 책을 추가합니다.
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
+    }
 
     @Test
     public void bookName_Cinderella_verify(){
@@ -60,7 +79,61 @@ public class MockServiceTest{
 
     }
 
+    @Test //가격 변동
+    public void Check_set_AuthorandPriceMock(){
+
+        when(mockService.findByName("Pinocchio")).thenReturn(new Book("Pinocchio", "CarloCollodi",12500));
+        when(mockService.updatePrice_ByName("Pinocchio",20000)).thenReturn(new Book("Pinocchio", "CarloCollodi",20000));
+
+        Book book = mockService.updatePrice_ByName("Pinocchio",20000);
+
+        assertThat(book.getPrice(), is(20000));
+
+    }
+
+    // 책 리스트를 만들어 올바르게 저장되고 있는지 확인한다.
+    @Test
+    public void checkBookList(){
+        assertThat(books.get(2).getPrice(), is(12500));
+        assertThat(books.get(0).getName(), is("Cinderella"));
+        assertThat(books.get(1).getAuthor(), is("AlanAlexanderMilne"));
+    }
+
+    // 위의 테스트와 리스트를 같이 사용하여 test 해보기
+    @Test
+    public void combineTwo(){
+        when(mockService.findByName("Cinderella")).thenReturn(new Book("Cinderella", "AlanAlexanderMilne", 15000));
+        assertThat(mockService.findByName("Cinderella").getPrice(), is(books.get(1).getPrice()));
+    }
+
+    //서점에서 "신데렐라"라는 책을 저장하려고하면 오류를 던진다.
+    //ex. "신데렐라"는 판매불가 책이라고 가정.
+    @Test(expected = IllegalArgumentException.class)
+    public void CheckStockBook(){
+        Book book = mock(Book.class);
+        doThrow(new IllegalArgumentException()).when(book).setName(eq("TheLittleMermaid"));
+        book.setName("TheLittleMermaid");
+    }
 
 
+    /*책팔기Test 손님이 원하는 책을말하고 돈을 내면 거스름돈 주기!*/
+    @Test
+    public void TestChange() {
+
+        when(mockService.findByName("WinnieThePooh")).thenReturn(new Book("WinnieThePooh", "AlanAlexanderMilne", 15000));
+
+        assertThat(mockService.Buy_Book_change("WinnieThePooh",20000), is(5000));
+
+    }
+
+    /*손님이 돈을 덜 낸 경우 Test 에러 처리 될까요~?? 된다1!!!*/
+    @Test(expected = IllegalArgumentException.class)
+    public void exeptionTestChange(){
+
+        when(mockService.findByName("WinnieThePooh")).thenReturn(new Book("WinnieThePooh", "AlanAlexanderMilne", 15000));
+
+        assertThat(mockService.Buy_Book_change("WinnieThePooh",5000), is(-10000));
+
+    }
 
 }
